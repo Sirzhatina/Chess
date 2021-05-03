3c3ef75cab7bbdc0348b2c8bb33a47a4972d0f10
Sirzhatina <mazurkevichsergey@gmail.com>
2021-05-03 22:28:05 +0300

    enums started from 0, not 1

--- Square.h
+++ Square.h
@@ -5,8 +5,8 @@
 #ifndef CHESS_SQUARE_H
 #define CHESS_SQUARE_H
 
-enum class Horizontal { A = 1, B, C, D, E, F, G, H };
-enum class Vertical { one = 1, two, three, four, five, six, seven, eight };
+enum class Horizontal { A, B, C, D, E, F, G, H };
+enum class Vertical { one, two, three, four, five, six, seven, eight };
 
 class Square {
     Horizontal x;
