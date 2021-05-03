b0d738d91d11a77ee0330117409082d4af17ab0d
Sirzhatina <mazurkevichsergey@gmail.com>
2021-05-03 14:09:14 +0300

    x and y enums moved from Piece.h to Square.h

--- Square.h
+++ Square.h
@@ -5,8 +5,12 @@
 #ifndef CHESS_SQUARE_H
 #define CHESS_SQUARE_H
 
+enum class Horizontal { A = 1, B, C, D, E, F, G, H };
+enum class Vertical { one = 1, two, three, four, five, six, seven, eight };
 
 class Square {
+    Horizontal x;
+    Vertical y;
 
 };
 
