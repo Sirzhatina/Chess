aeeb4412ea171ceafcc5a7471a07ae4f48b2a2cc
Sirzhatina <mazurkevichsergey@gmail.com>
2021-05-10 01:46:59 +0300

    added pointer to square's piece and constructor

--- Square.h
+++ Square.h
@@ -6,16 +6,17 @@
 #define CHESS_SQUARE_H
 
 #include <memory>
-#include "Piece/Piece.h"
 
 enum class Horizontal { A, B, C, D, E, F, G, H };
 enum class Vertical { one, two, three, four, five, six, seven, eight };
 
+class Piece;
 class Square {
     Horizontal x;
     Vertical y;
-
-    std::unique_ptr<Piece> piece;
+    std::shared_ptr<Piece> piece;
+public:
+    Square(Horizontal X, Vertical Y, std::shared_ptr<Piece> p = nullptr): x(X), y(Y), piece(std::move(p)) { }
 
 };
 
