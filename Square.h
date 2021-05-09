ec560ebcc2d04c1c7713fd9017432a441146b35a
Sirzhatina <mazurkevichsergey@gmail.com>
2021-05-10 01:29:28 +0300

    added piece object to class Square

--- Square.h
+++ Square.h
@@ -5,12 +5,17 @@
 #ifndef CHESS_SQUARE_H
 #define CHESS_SQUARE_H
 
+#include <memory>
+#include "Piece/Piece.h"
+
 enum class Horizontal { A, B, C, D, E, F, G, H };
 enum class Vertical { one, two, three, four, five, six, seven, eight };
 
 class Square {
     Horizontal x;
     Vertical y;
+
+    std::unique_ptr<Piece> piece;
 
 };
 
