2272c9af80b96f3b4943ac229ff195338a27e1ae
Sirzhatina <mazurkevichsergey@gmail.com>
2021-08-10 11:49:42 +0300

    Board keeps pointers to squares now

--- Board.cpp
+++ Board.cpp
@@ -2,3 +2,24 @@
 // Created by Sirzh on 10.05.2021.
 //
 
+#include "Board.h"
+
+
+Board::Board()
+{
+    int hor = 1;
+    int vert = 8;
+    bool color = false;
+
+    for (auto& arr: board)
+    {
+        for (auto& sqr: arr)
+        {
+            sqr = new Square(Horizontal(hor++), Vertical(vert), Color(color));
+            color = !color;
+        }
+        color = !color;
+        hor = 1;
+        vert--;
+    }
+}
--- Board.h
+++ Board.h
@@ -5,4 +5,20 @@
 #ifndef CHESS_BOARD_H
 #define CHESS_BOARD_H
 
+#include <array>
+#include "Traits.h"
+#include "Square.h"
+
+class Player;
+
+class Board {
+    static constexpr auto SIZE = 8;
+
+    std::array<std::array<Square*, SIZE>, SIZE> board;
+public:
+    Board();
+    ~Board();
+    void setPieces(const Player& pl);
+};
+
 #endif //CHESS_BOARD_H
