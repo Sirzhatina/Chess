a71d42ea8273688d05f8ac5eaddace80ed396356
Sirzhatina <mazurkevichsergey@gmail.com>
2021-08-10 11:52:15 +0300

    Empty

--- Square.cpp
+++ Square.cpp
@@ -3,19 +3,3 @@
 //
 
 #include "Square.h"
-#include <cctype>
-
-
-bool Square::convertCoord(char horiz, char vert)
-{
-    horiz = tolower(horiz);
-
-    if (horiz < 'a' || horiz > 'h' || vert < '1' || vert > '8')
-    {
-        return false;
-    }
-    this->x = static_cast<Horizontal>(horiz - 'a' + 1);
-    this->y = static_cast<Vertical>(vert - '1' + 1);
-
-    return true;
-}
