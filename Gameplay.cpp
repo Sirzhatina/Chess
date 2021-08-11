3e633ca1f400371669a3b1501f8defb4df7cf334
Sirzhatina <mazurkevichsergey@gmail.com>
2021-08-12 00:48:04 +0300

    start() gets additional functionality

--- Gameplay.cpp
+++ Gameplay.cpp
@@ -8,15 +8,25 @@ int Gameplay::start()
 
     Traits::Coordinates from, to;
 
+    auto moves = &white;
+    auto notMoves = &black;
+
     while (showGoesOn())
     {
-        std::cout << "Enter coordinates to move to and from: ";
+        std::cout << "Enter coordinates to move from and to: ";
         std::cin.get(xFrom).get(yFrom);
         std::cin.get(xTo).get(yTo);
 
-        from = convertCoordinates(xFrom - 'a' - 1, yFrom - '0');
-        to = convertCoordinates(xTo - 'a' - 1, yTo - '0');
+        from = convertCoordinates(xFrom - 'a', yFrom - '1');
+        to = convertCoordinates(xTo - 'a', yTo - '1');
 
+        if (isPossibleMove(from, to))
+        {
+            moves->move(from, to);
 
+            
+        }
+
+        whiteMove = !whiteMove;
     }
 }
