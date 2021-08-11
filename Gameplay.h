903a3b42bf5b4cdf13698a4a221f54368141606e
Sirzhatina <mazurkevichsergey@gmail.com>
2021-08-11 23:34:45 +0300

    new gameplay class

---
+++ Gameplay.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Traits.h"
+#include "Square.h"
+#include "Piece/Piece.h"
+#include "Player.h"
+#include "Board.h"
+
+
+class Gameplay
+{
+    Board board;
+    Player white{ &board, Traits::Color::WHITE };
+    Player black{ &board, Traits::Color::BLACK };
+
+    bool whiteMove{ true };
+
+    bool isPossibleMove() const;
+public:
+    Gameplay() = default;
+
+    bool showGoesOn() { return !white.isCheckmate() && !black.isCheckmate(); }
+};
