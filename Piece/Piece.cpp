d922508b35da58cd95baf2669bacc175cc7ef1c6
Sirzhatina <mazurkevichsergey@gmail.com>
2021-08-13 17:44:19 +0300

    Updated interfaces of the classes

--- Piece/Piece.cpp
+++ Piece/Piece.cpp
@@ -6,10 +6,16 @@
 #include "..\Player.h"
 
 
-Piece::~Piece() = default;
-
 Piece::Piece(const Player* p, Traits::Color c, Square* sqr)
 : player(p)
-, color(p->getColor())
 , currentSqr(sqr)
 { }
+
+
+void Pawn::move(Traits::Coordinates to)
+{
+    if (firstMove)
+    {
+        
+    }
+}
--- Piece/Piece.h
+++ Piece/Piece.h
@@ -12,16 +12,16 @@ class Player;
 
 class Piece {
     const Player* player;
-    const Traits::Color color;
     Square* currentSqr;
 public:
     Piece(const Player* p, Traits::Color c, Square* sqr);
 
-    virtual ~Piece() = 0;
+    virtual ~Piece() = default;
 
-    virtual void Move();
+    virtual void move(Traits::Coordinates to) = 0;
 
-    virtual Square getCoord() const;
+    const Player* getPlayer() const { return player; }
+    const Square* getCoord() const { return currentSqr; }
 };
 
 class Pawn: public Piece {
@@ -29,36 +29,44 @@ class Pawn: public Piece {
 public:
     Pawn(Player* p, Traits::Color c, Square* sqr): Piece(p, c, sqr) { }
 
-    void Move() final;
+    void move(Traits::Coordinates to) override;
 };
 
 
 class Knight: public Piece {
 public:
     Knight(Player* p, Traits::Color c, Square* sqr);
+
+    void move(Traits::Coordinates to) override;
 };
 
 class Bishop: public Piece {
 public:
     Bishop(Player* p, Traits::Color c, Square* sqr);
+
+    void move(Traits::Coordinates to) override;
 };
 
 class Rook: public Piece {
 public:
-    Rook(Player* , Traits::Color c, Square* sqr);
+    Rook(Player* p, Traits::Color c, Square* sqr);
+
+    void move(Traits::Coordinates to) override;
 };
 
 class Queen: public Piece {
 public:
     Queen(Player* p, Traits::Color c, Square* sqr);
+
+    void move(Traits::Coordinates to) override;
 };
 
 class King: public Piece {
 public:
     King(Player* p, Traits::Color c, Square* sqr);
+
+    void move(Traits::Coordinates to) override;
 };
-
-
 
 
 #endif //CHESS_PIECE_H
