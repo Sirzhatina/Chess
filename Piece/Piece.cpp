44ad498aa28b8ecb5546a930a89678f7b248a377
Sirzhatina <mazurkevichsergey@gmail.com>
2021-08-16 22:21:56 +0300

    defined errMsg and implemented possibleMove() function in all derived classes

--- Piece/Piece.cpp
+++ Piece/Piece.cpp
@@ -2,19 +2,96 @@
 // Created by Sirzh on 15.04.2021.
 //
 
+#include <stdexcept>
+#include <cstdlib>
 #include "Piece.h"
+#include "..\Player.h"
+#include "..\Square.h"
+#include "Board.h"
 
+std::string Piece::errMsg = "Impossible to move";
 
-Piece::Piece(const Player* p, Traits::Color c, Square* sqr)
+Piece::Piece(const Player* p, Traits::Coordinates coord)
 : player(p)
-, currentSqr(sqr)
+, color(p->getColor())
+, currentCoord(coord)
 { }
 
 
-void Pawn::move(Traits::Coordinates to)
+bool Pawn::possibleMove(Traits::Coordinates to)
 {
-    if (firstMove)
+    auto from = getCoord();
+    if (getPlayer()->getBoard()->getPiece(to) != nullptr)
     {
-        
+        if (getColor() != Traits::Color::WHITE && whiteAttack(from, to) ||
+            getColor() != Traits::Color::BLACK && blackAttack(from, to) ||
+            !whiteAttack(from, to) && !blackAttack(from, to))
+        {
+            return false;
+        }
     }
+    else if (firstMove && int(to.v) - int(from.v) > std::abs(2) ||
+            int(to.v) - int(from.v) > std::abs(1)               ||
+            to.h != from.h)
+    {
+        return false;
+    }
+    firstMove = false;
+    return true;
+}
+
+bool Knight::possibleMove(Coordinates to)
+{
+    auto from = getCoord();
+    if (!(int(from.v) - int(to.v) == std::abs(2) && int(from.h) - int(to.h) == std::abs(1)) ||
+        !(int(from.h) - int(to.h) == std::abs(2) && int(from.v) - int(to.v) == std::abs(1)))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+bool Bishop::possibleMove(Coordinates to)
+{
+    auto from = getCoord();
+    if (std::abs(int(to.h) - int(from.h)) != std::abs(int(to.v) - int(from.v)))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+bool Rook::possibleMove(Coordinates to)
+{
+    auto from = getCoord();
+    if (from.h != to.h && from.v != to.v)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+bool Queen::possibleMove(Coordinates to)
+{
+    auto from = getCoord();
+    if (from.h != to.h && from.v != to.v ||
+        std::abs(int(to.h) - int(from.h)) != std::abs(int(to.v) - int(from.v)))
+    {
+        return false;
+    }
+    return true;
+}
+
+
+bool King::possibleMove(Coordinates to)
+{
+    auto from = getCoord();
+    if (std::abs(int(to.h) - int(from.h)) > 1 || std::abs(int(to.v) - int(from.v)) > 1)
+    {
+        return false;
+    }
+    return true;
 }
--- Piece/Piece.h
+++ Piece/Piece.h
@@ -6,67 +6,80 @@
 #define CHESS_PIECE_H
 
 #include "..\Traits.h"
+#include <string>
 
 class Square;
 class Player;
 
+
 class Piece {
+protected:
+    using Coordinates = Traits::Coordinates;
+    static std::string errMsg;
+    void setCoordinates(Coordinates coord) { currentCoord = coord; }
+
+private:
     const Player* player;
-    Square* currentSqr;
+    const Traits::Color color;
+    Coordinates currentCoord;
+
 public:
-    Piece(const Player* p, Traits::Color c, Square* sqr);
+    Piece(const Player* p, Coordinates coord);
 
     virtual ~Piece() = default;
 
-    virtual void move(Traits::Coordinates to) = 0;
+    virtual bool possibleMove(Traits::Coordinates to) = 0;
 
     const Player* getPlayer() const { return player; }
-    const Square* getCoord() const { return currentSqr; }
+    Traits::Color getColor() const { return color; }
+    Traits::Coordinates getCoord() const { return currentCoord; }
 };
 
 class Pawn: public Piece {
     bool firstMove{ true };
-public:
-    Pawn(Player* p, Traits::Color c, Square* sqr): Piece(p, c, sqr) { }
 
-    void move(Traits::Coordinates to) override;
+    bool whiteAttack(Coordinates from, Coordinates to) const { return int(to.v) - int(from.v) == abs(int(to.h) - int(from.h)); }
+    bool blackAttack(Coordinates from, Coordinates to) const { return int(from.v) - int(to.v) == abs(int(to.h) - int(from.h)); }
+public:
+    Pawn(Player* p, Traits::Coordinates coord): Piece(p, coord) { }
+
+    bool possibleMove(Traits::Coordinates to) override;
 };
 
 
 class Knight: public Piece {
 public:
-    Knight(Player* p, Traits::Color c, Square* sqr);
+    Knight(Player* p, Traits::Coordinates coord);
 
-    void move(Traits::Coordinates to) override;
+    bool possibleMove(Traits::Coordinates to) override;
 };
 
 class Bishop: public Piece {
 public:
-    Bishop(Player* p, Traits::Color c, Square* sqr);
+    Bishop(Player* p, Traits::Coordinates coord);
 
-    void move(Traits::Coordinates to) override;
+    bool possibleMove(Traits::Coordinates to) override;
 };
 
 class Rook: public Piece {
 public:
-    Rook(Player* p, Traits::Color c, Square* sqr);
+    Rook(Player* p, Traits::Coordinates coord);
 
-    void move(Traits::Coordinates to) override;
+    bool possibleMove(Traits::Coordinates to) override;
 };
 
 class Queen: public Piece {
 public:
-    Queen(Player* p, Traits::Color c, Square* sqr);
+    Queen(Player* p, Traits::Coordinates coord);
 
-    void move(Traits::Coordinates to) override;
+    bool possibleMove(Traits::Coordinates to) override;
 };
 
 class King: public Piece {
 public:
-    King(Player* p, Traits::Color c, Square* sqr);
+    King(Player* p, Traits::Coordinates coord);
 
-    void move(Traits::Coordinates to) override;
+    bool possibleMove(Traits::Coordinates to) override;
 };
-
 
 #endif //CHESS_PIECE_H
