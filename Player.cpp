d867d08f5b3c930c9e093562e05473897156e06e
Sirzhatina <mazurkevichsergey@gmail.com>
2021-08-16 22:24:15 +0300

    reimplemented constructor and added (incompleted) function move()

--- Player.cpp
+++ Player.cpp
@@ -6,6 +6,7 @@
 #include "Piece\Piece.h"
 #include "Board.h"
 #include "Square.h"
+#include <typeinfo>
 
 
 Player::Player(Board* b, Traits::Color c)
@@ -14,35 +15,46 @@ Player::Player(Board* b, Traits::Color c)
 {
     board->addPlayer(this);
 
-    Traits::Vertical startOfPawns = color == Traits::Color::WHITE ? Traits::Vertical::two : Traits::Vertical::seven;
-    Traits::Vertical startOfOthers = color == Traits::Color::WHITE ? Traits::Vertical::one : Traits::Vertical::eight;
+    Traits::Vertical startOfPawns = (color == Traits::Color::WHITE) ? Traits::Vertical::two : Traits::Vertical::seven;
+    Traits::Vertical startOfOthers = (color == Traits::Color::WHITE) ? Traits::Vertical::one : Traits::Vertical::eight;
 
     unsigned alongLine = 0;
     for (auto& pwn: pawn)
     {
-        pwn = new Pawn{this, color, &board->getSquare(Traits::Coordinates{ Traits::Horizontal(alongLine++), startOfPawns }) };
+        pwn = new Pawn{ this, Traits::Coordinates{ Traits::Horizontal{ alongLine }, startOfPawns } };
+        board->setPiece(pwn, Traits::Coordinates{ Traits::Horizontal{ alongLine++ }, startOfPawns });
     }
 
-    alongLine = 0;
-    for (unsigned i = 0, inc = 1; i < PAIR_PIECES; i++, alongLine += 6, inc *= -1)
+    alongLine = -1;
+    for (unsigned i = 0, inc = 1; i < PAIR_PIECES; i++)
     {
-        rook[i] = new Rook{ this, color, &board->getSquare(Traits::Coordinates{ Traits::Horizontal(alongLine += inc), startOfOthers }) };
-        knight[i] = new Knight{ this, color, &board->getSquare(Traits::Coordinates{ Traits::Horizontal(alongLine += inc), startOfOthers }) };
-        bishop[i] = new Bishop{ this, color, &board->getSquare(Traits::Coordinates{ Traits::Horizontal(alongLine += inc), startOfOthers }) };
+        rook[i] = new Rook{ this, Traits::Coordinates{ Traits::Horizontal{ alongLine }, startOfOthers } };
+        board->setPiece(rook[i], Traits::Coordinates{ Traits::Horizontal{ alongLine += inc }, startOfOthers} );
+
+        knight[i] = new Knight{ this, Traits::Coordinates{ Traits::Horizontal{ alongLine }, startOfOthers } };
+        board->setPiece(knight[i], Traits::Coordinates{ Traits::Horizontal{ alongLine += inc }, startOfOthers });
+
+        bishop[i] = new Bishop{ this, Traits::Coordinates{ Traits::Horizontal{ alongLine }, startOfOthers } };
+        board->setPiece(bishop[i], Traits::Coordinates{ Traits::Horizontal{ alongLine += inc }, startOfOthers });
+
+        inc *= -1;
+        alongLine += 6;
     }
 
     // Queen prefers corresponding color
-    queen = new Queen{ this, color, &board->getSquare(Traits::Coordinates{ Traits::Horizontal::D, startOfOthers }) };
-    king = new King{ this, color, &board->getSquare(Traits::Coordinates{ Traits::Horizontal::E, startOfOthers }) };
+    queen = new Queen{ this, Traits::Coordinates{ Traits::Horizontal::D, startOfOthers } };
+    king = new King{ this, Traits::Coordinates{ Traits::Horizontal::E, startOfOthers } };
 }
 
 void Player::move(Traits::Coordinates from, Traits::Coordinates to)
 {
-    auto fromSquare = &board->getSquare(from);
-    auto piece = &fromSquare->getPiece();
-
+    auto piece = board->getPiece(from);
     if (this == piece->getPlayer())
     {
-        piece->move(to);
+        if (board->getSquare(to)->piece->getPlayer() != this)
+        {
+            // instead of searching for pointer with corresponding address inside of Player, we use const_cast<>()
+            board->setPiece(const_cast<Piece*>(piece), to);
+        }
     }
 }
