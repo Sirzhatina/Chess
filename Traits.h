7c63a3bf1eb8f61717dc9863290ef2e1ab89e802
Sirzhatina <mazurkevichsergey@gmail.com>
2021-08-18 16:42:34 +0300

    overloaded operators for Coordinates

--- Traits.h
+++ Traits.h
@@ -23,6 +23,14 @@ struct Coordinates {
     Horizontal x;
     Vertical y;
 };
+bool operator==(const Coordinates& c1, const Coordinates& c2)
+{
+    return c1.x == c2.x && c1.y == c2.y;
+}
+bool operator!=(const Coordinates& c1, const Coordinates& c2)
+{
+    return !(c1 == c2);
+}
 
 enum class Color {
     BLACK, WHITE
