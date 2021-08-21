38280e70ec603218ab027254c8d076bb4ae6e280
Sirzhatina <mazurkevichsergey@gmail.com>
2021-08-21 18:50:15 +0300

    totally changed interface, implemented static methods

--- Piece/Piece.cpp
+++ Piece/Piece.cpp
@@ -18,63 +18,53 @@ Piece::Piece(const Player* p, Traits::Coordinates coord)
 { }
 
 
-bool Pawn::possibleMove(Traits::Coordinates to) const
-{
-    if (firstMove && int(to.y) - int(getCoord().y) > std::abs(2) ||
-        !firstMove && int(to.y) - int(getCoord().y) > std::abs(1) ||
-        to.x != getCoord().x)
-    {
-        if (!possibleAttack(to))
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
 bool Pawn::possibleAttack(Traits::Coordinates to) const 
 {
     int move = int(to.y) - int(getCoord().y);
     return (getColor() == Traits::Color::BLACK ? move == -1 : move == 1) && 
-                                 std::abs(int(to.x) - int(getCoord().x)) == 1  && 
-                                    getBoard()->getPiece(to) != nullptr; 
+            std::abs(int(to.x) - int(getCoord().x)) == 1                 && 
+            getBoard()->getPiece(to) != nullptr; 
 }
 
-void Pawn::move(Traits::Coordinates to)
+bool Pawn::correctRoute(const Pawn& p, Traits::Coordinates to)
 {
-    if (!possibleMove(to))
+    int move = int(to.y) - int(p.getCoord().y);
+    bool correctDirection = (move > 0) ? p.getColor() == Traits::Color::WHITE : p.getColor() == Traits::Color::BLACK;
+
+    if ((p.firstMove ? std::abs(move) <= 2 : std::abs(move) <= 1) && 
+         to.x == p.getCoord().x                                   &&
+         correctDirection)
     {
-        throw std::runtime_error{ errMsg };
+        if (std::abs(move) == 2)
+        {
+            Traits::Coordinates coor{ to.x, (move > 0) ? Traits::Vertical{ int(to.y) - 1 } : Traits::Vertical{ int(to.y) + 1 } };
+            if (p.getBoard()->getPiece(coor) == nullptr)
+            {
+                return true;
+            }
+        }
+        else
+        {
+            return true;
+        }
     }
-    setCoordinates(to);
-    getBoard()->setPiece(this, to);
+    return false;
 }
 
-bool Knight::possibleMove(Traits::Coordinates to) const
+bool Knight::correctRoute(Traits::Coordinates from, Traits::Coordinates to, const Board* b)
 {
-    if (!(std::abs(int(getCoord().y) - int(to.y)) == 2 && std::abs(int(getCoord().x) - int(to.x)) == 1) ||
-        !(std::abs(int(getCoord().x) - int(to.x)) == 2 && std::abs(int(getCoord().y) - int(to.y)) == 1))
+    if ((std::abs(int(to.y) - int(from.y)) == 2 && std::abs(int(to.x) - int(from.x)) == 1) ||
+        (std::abs(int(to.x) - int(from.x)) == 2 && std::abs(int(to.y) - int(from.y)) == 1))
     {
-        return false;
+        return true;
     }
-
-    return true;
+    return false;
 }
 
-void Knight::move(Traits::Coordinates to)
+bool Bishop::correctRoute(Traits::Coordinates from, Traits::Coordinates to, const Board* b)
 {
-    if (!possibleMove(to))
-    {
-        throw std::runtime_error{ errMsg };
-    }
-    setCoordinates(to);
-    getBoard()->setPiece(this, to);
-}
-
-bool Bishop::possibleMove(Traits::Coordinates to) const
-{
-    int diffX = int(to.x) - int(getCoord().x);
-    int diffY = int(to.y) - int(getCoord().y);
+    int diffX = int(to.x) - int(from.x);
+    int diffY = int(to.y) - int(from.y);
 
     if (std::abs(diffX) != std::abs(diffY))
     {
@@ -83,43 +73,31 @@ bool Bishop::possibleMove(Traits::Coordinates to) const
     int incX = (diffX > 0) ? 1 : -1;
     int incY = (diffY > 0) ? 1 : -1;
 
-    for (auto coor = getCoord(); coor != to; )
+    for (auto coor = from; coor != to; )
     {
         coor.x = Traits::Horizontal(int(coor.x) + incX); 
         coor.y = Traits::Vertical(int(coor.y) + incY);
-        if (getBoard()->getPiece(coor) != nullptr)
+        if (b->getPiece(coor) != nullptr)
         {
             return false;
         }
     }
-
-    return true;
 }
 
-void Bishop::move(Traits::Coordinates to)
+bool Rook::correctRoute(Traits::Coordinates from, Traits::Coordinates to, const Board* b)
 {
-    if (!possibleMove(to))
-    {
-        throw std::runtime_error{ errMsg };
-    }
-    setCoordinates(to);
-    getBoard()->setPiece(this, to);
-}
-
-bool Rook::possibleMove(Traits::Coordinates to) const
-{
-    if (getCoord().x != to.x && getCoord().y != to.y)
+    if (from.x != to.x && from.y != to.y)
     {
         return false;
     }
 
-    if (getCoord().x != to.x)
+    if (from.x != to.x)
     {
-        int incX = int(to.x) - int(getCoord().x) > 0 ? 1 : -1;
-        for (auto coor = getCoord(); coor != to; )
+        int incX = int(to.x) - int(from.x) > 0 ? 1 : -1;
+        for (auto coor = from; coor != to; )
         {
             coor.x = Traits::Horizontal(int(coor.x) + incX);
-            if (getBoard()->getPiece(coor) != nullptr)
+            if (b->getPiece(coor) != nullptr)
             {
                 return false;
             }
@@ -127,114 +105,29 @@ bool Rook::possibleMove(Traits::Coordinates to) const
     }
     else
     {
-        int incY = int(to.y) - int(getCoord().y) > 0 ? 1 : -1;
-        for (auto coor = getCoord(); coor != to; )
+        int incY = int(to.y) - int(from.y) > 0 ? 1 : -1;
+        for (auto coor = from; coor != to; )
         {
             coor.y = Traits::Vertical(int(coor.y) + incY);
-            if (getBoard()->getPiece(to) != nullptr)
+            if (b->getPiece(to) != nullptr)
             {
                 return false;
-            }
-        }
-    }
-
-    return true;
-}
-
-void Rook::move(Traits::Coordinates to)
-{
-    if (!possibleMove(to))
-    {
-        throw std::runtime_error{ errMsg };
-    }
-    setCoordinates(to);
-    getBoard()->setPiece(this, to);
-}
-
-bool Queen::possibleMove(Traits::Coordinates to) const
-{
-    if (getCoord().x != to.x && getCoord().y != to.y)   // if true, we deal with not rook - meaning, of course, bishop
-    {
-        int diffX = int(to.x) - int(getCoord().x);
-        int diffY = int(to.y) - int(getCoord().y);
-
-        if (std::abs(diffX) != std::abs(diffY))
-        {
-            return false;   // if not rook and not bishop, we return false
-        }
-
-        int incX = (diffX > 0) ? 1 : -1;
-        int incY = (diffY > 0) ? 1 : -1;
-
-        Traits::Coordinates coor{ Traits::Horizontal(int(getCoord().x) + incX), Traits::Vertical(int(getCoord().y) + incY) };
-        while (coor != to)
-        {
-            if (getBoard()->getPiece(coor) != nullptr) 
-            {
-                return false;
-            }
-            coor.x = Traits::Horizontal(int(coor.x) + incX);
-            coor.y = Traits::Vertical(int(coor.y) + incY);
-        }
-    }
-    else // consequently, here we deal with rook
-    {
-        int inc = (getCoord().x != to.x) ? ((int(to.x) - int(getCoord().x) > 0) ? 1 : -1) : 
-                                           ((int(to.y) - int(getCoord().y) > 0) ? 1 : -1);
-        if (getCoord().x != to.x)
-        {
-            Traits::Coordinates coor{ Traits::Horizontal(int(getCoord().x) + inc), getCoord().y };
-            while (coor != to)
-            {
-                if (getBoard()->getPiece(coor) != nullptr)
-                {
-                    return false;
-                }
-                coor.x = Traits::Horizontal(int(coor.x) + inc);
-            }
-        }
-        else
-        {
-            Traits::Coordinates coor{ getCoord().x, Traits::Vertical(int(getCoord().y) + inc) };
-            while (coor != to)
-            {
-                if (getBoard()->getPiece(coor) != nullptr)
-                {
-                    return false;
-                }
-                coor.y = Traits::Vertical(int(coor.y) + inc);
             }
         }
     }
     return true;
 }
 
-void Queen::move(Traits::Coordinates to)
+bool Queen::correctRoute(Traits::Coordinates from, Traits::Coordinates to, const Board* b)
 {
-    if (!possibleMove(to))
-    {
-        throw std::runtime_error{ errMsg };
-    }
-    setCoordinates(to);
-    getBoard()->setPiece(this, to);
+    return Bishop::correctRoute(from, to, b) || Rook::correctRoute(from, to, b);
 }
 
-
-bool King::possibleMove(Traits::Coordinates to) const
+bool King::correctRoute(Traits::Coordinates from, Traits::Coordinates to, const Board* b)
 {
-    if (std::abs(int(to.x) - int(getCoord().x)) > 1 || std::abs(int(to.y) - int(getCoord().y)) > 1)
+    if (std::abs(int(to.x) - int(from.x)) > 1 || std::abs(int(to.y) - int(from.y)) > 1)
     {
         return false;
     }
     return true;
-}
-
-void King::move(Traits::Coordinates to)
-{
-    if (!possibleMove(to))
-    {
-        throw std::runtime_error{ errMsg };
-    }
-    setCoordinates(to);
-    getBoard()->setPiece(this, to);
 }
--- Piece/Piece.h
+++ Piece/Piece.h
@@ -15,78 +15,77 @@ class Player;
 class Piece {
 protected:
     static std::string errMsg;
-    void setCoordinates(Traits::Coordinates coord) { currentCoord = coord; }
 
 private:
-    const Player* player;
-    Board* board;
+    const Player*       player;
+    Board*              board;
     const Traits::Color color;
     Traits::Coordinates currentCoord;
 
 public:
     Piece(const Player* p, Traits::Coordinates coord);
-
     virtual ~Piece() = default;
 
     virtual bool possibleMove(Traits::Coordinates to) const = 0;
-    virtual void move(Traits::Coordinates to) = 0;
+    
+    void setCoordinates(Traits::Coordinates coord) { currentCoord = coord; }
 
-    const Player* getPlayer() const { return player; }
-    Board* getBoard() const { return board; }
-    Traits::Color getColor() const { return color; }
-    Traits::Coordinates getCoord() const { return currentCoord; }
+    const Player*       getPlayer() const { return player; }
+    Board*              getBoard()  const { return board; }
+    Traits::Color       getColor()  const { return color; }
+    Traits::Coordinates getCoord()  const { return currentCoord; }
 };
 
 class Pawn: public Piece {
     bool firstMove{ true };
-
     bool possibleAttack(Traits::Coordinates to) const;
-public:
-    Pawn(Player* p, Traits::Coordinates coord): Piece(p, coord) { }
 
-    bool possibleMove(Traits::Coordinates to) const override;
-    void move(Traits::Coordinates to) override;
+public:
+    Pawn(const Player* p, Traits::Coordinates coord): Piece(p, coord) { }
+
+    bool possibleMove(Traits::Coordinates to) const override { return possibleAttack(to) || correctRoute(*this, to); }
+    static bool correctRoute(const Pawn& p, Traits::Coordinates to);
 };
 
 
 class Knight: public Piece {
 public:
-    Knight(Player* p, Traits::Coordinates coord);
+    Knight(const Player* p, Traits::Coordinates coord);
 
-    bool possibleMove(Traits::Coordinates to) const override;
-    void move(Traits::Coordinates to) override;
+    bool possibleMove(Traits::Coordinates to) const override { return correctRoute(getCoord(), to); }
+    static bool correctRoute(Traits::Coordinates from, Traits::Coordinates to, const Board* b = nullptr);
 };
 
 class Bishop: public Piece {
 public:
-    Bishop(Player* p, Traits::Coordinates coord);
+    Bishop(const Player* p, Traits::Coordinates coord);
 
-    bool possibleMove(Traits::Coordinates to) const override;
-    void move(Traits::Coordinates to) override;
+    bool possibleMove(Traits::Coordinates to) const override { return correctRoute(getCoord(), to, getBoard()); }
+    static bool correctRoute(Traits::Coordinates from, Traits::Coordinates to, const Board* b);
 };
 
 class Rook: public Piece {
 public:
-    Rook(Player* p, Traits::Coordinates coord);
+    Rook(const Player* p, Traits::Coordinates coord);
 
-    bool possibleMove(Traits::Coordinates to) const override;
-    void move(Traits::Coordinates to) override;
+    bool possibleMove(Traits::Coordinates to) const override { return correctRoute(getCoord(), to, getBoard()); }
+    static bool correctRoute(Traits::Coordinates from, Traits::Coordinates to, const Board* b);
 };
 
 class Queen: public Piece {
 public:
-    Queen(Player* p, Traits::Coordinates coord);
+    Queen(const Player* p, Traits::Coordinates coord);
 
-    bool possibleMove(Traits::Coordinates to) const override;
-    void move(Traits::Coordinates to) override;
+    bool possibleMove(Traits::Coordinates to) const override { return correctRoute(getCoord(), to, getBoard()); }
+    static bool correctRoute(Traits::Coordinates from, Traits::Coordinates to, const Board* b);
 };
 
 class King: public Piece {
 public:
-    King(Player* p, Traits::Coordinates coord);
+    King(const Player* p, Traits::Coordinates coord);
 
-    bool possibleMove(Traits::Coordinates to) const override;
-    void move(Traits::Coordinates to) override;
+    bool possibleMove(Traits::Coordinates to) const override { return correctRoute(getCoord(), to); }
+    static bool correctRoute(Traits::Coordinates from, Traits::Coordinates to, const Board* b = nullptr);
 };
 
 #endif //CHESS_PIECE_H
