b88ba83d79f8649204c5b9734c5d1f389805f08e
Sirzhatina <mazurkevichsergey@gmail.com>
2021-08-28 18:01:54 +0300

    Gameplay contains pointer to Board now

--- Gameplay.h
+++ Gameplay.h
@@ -8,21 +8,21 @@
 
 class Gameplay
 {
-    Board board;
-    Player white{ &board, Traits::Color::WHITE };
-    Player black{ &board, Traits::Color::BLACK };
+    Board* board;
+    Player white{ board, Traits::Color::WHITE };
+    Player black{ board, Traits::Color::BLACK };
 
     bool whiteMove{ true };
 
     bool showGoesOn() { return !white.isCheckmate() && !black.isCheckmate(); }
     void inputToMove(Traits::Coordinates& from, Traits::Coordinates& to) const;
 
-    bool possibleMove(const Player* moves, const Player* checks, Traits::Coordinates from, Traits::Coordinates to) const;
+    bool possibleMove(Player* moves, Player* checks, Traits::Coordinates from, Traits::Coordinates to) const;
     
     static Traits::Coordinates convertCoordinates(int x, int y) { return { Traits::Horizontal{ x }, Traits::Vertical{ y } }; }
 
 public:
-    Gameplay() = default;
+    Gameplay(Board* b);
 
     int start();
 };
