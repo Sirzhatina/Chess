f7c69ba60dd3f1e040e14b62eb9017482bf66e5f
Sirzhatina <mazurkevichsergey@gmail.com>
2021-09-01 18:08:19 +0300

    new methods in Gameplay and Gameplay::start() is split into smaller methods

--- Gameplay.cpp
+++ Gameplay.cpp
@@ -50,7 +50,7 @@ bool Gameplay::possibleMove(Player* moves, Player* checks, Traits::Coordinates f
     return result;
 }
 
-bool Gameplay::isCheckmate(Player* checks, Player* inCheck)
+bool Gameplay::isCheckmate(Player* checks, Player* inCheck) const
 {
     std::vector<Piece*> checkPieces = checks->piecesAccessingSquare(inCheck->getKingCoord());  // containts 0, 1 or 2 pieces
     if (checkPieces.empty())
@@ -61,36 +61,12 @@ bool Gameplay::isCheckmate(Player* checks, Player* inCheck)
     {
         inCheck->setCheck(true);
     }
-    std::vector<Traits::Coordinates> squaresForKing;
+    std::vector<Traits::Coordinates> availableForKing = allSquaresForKing(inCheck, checks);
 
-    {
-        const Piece* pieceOnSquare{ nullptr };
-        Traits::Coordinates currentSquare;
-
-        for (int x = int(inCheck->getKingCoord().x) - 1; x < 3; x++)
-        {   
-            for (int y = int(inCheck->getKingCoord().y) - 1; y < 3; y++)
-            {
-                currentSquare = { Traits::Horizontal{x}, Traits::Vertical{y} };
-                pieceOnSquare = board->getPiece(currentSquare);
-
-                if (x > 0 && x < Traits::boardSize && y > 0 && y < Traits::boardSize &&
-                    inCheck->getKingCoord() != currentSquare)
-                {
-                    if (pieceOnSquare && pieceOnSquare->getColor() != inCheck->getColor() &&
-                        !checks->isAccessedSquare(currentSquare))
-                    {
-                        squaresForKing.push_back(currentSquare);
-                    }
-                }
-            }
-        }
-    }
-    if (checkPieces.size() > 1 && squaresForKing.empty())
+    if (checkPieces.size() > 1 && availableForKing.empty())
     {
         return true;
     }
-
     std::vector<Traits::Coordinates> allCheckSquares = checkPieces[0]->squaresBefore(inCheck->getKingCoord());
 
     const Piece* king = board->getPiece(inCheck->getKingCoord());
@@ -104,13 +80,65 @@ bool Gameplay::isCheckmate(Player* checks, Player* inCheck)
     return true;
 }
 
+std::vector<Traits::Coordinates> Gameplay::allSquaresForKing(const Player* moves, const Player* notMoves) const
+{
+    std::vector<Traits::Coordinates> result;
+
+    const Piece* pieceOnSquare{ nullptr };
+    Traits::Coordinates currentSquare;
+
+    for (int x = int(moves->getKingCoord().x) - 1, lim = x + 3; x < lim; x++)
+    {   
+        for (int y = int(moves->getKingCoord().y) - 1, lim = y + 3; y < lim; y++)
+        {
+            if (x > 0 && x < Traits::boardSize && y > 0 && y < Traits::boardSize)
+            {
+                currentSquare.x = Traits::Horizontal{x};
+                currentSquare.y = Traits::Vertical{y};
+                pieceOnSquare = board->getPiece(currentSquare);
+
+                if (pieceOnSquare && pieceOnSquare->getColor() != moves->getColor() &&
+                    moves->getKingCoord() != currentSquare &&
+                    !notMoves->isAccessedSquare(currentSquare))
+                {
+                    result.push_back(currentSquare);
+                }
+            }
+        }
+    }
+    return result;
+}
+
+bool Gameplay::kingCanMove(const Player* moves, const Player* notMoves) const
+{
+    const Piece* pieceOnSquare{ nullptr };
+    Traits::Coordinates currentSquare;
+
+    for (int x = int(moves->getKingCoord().x) - 1, lim = x + 3; x < lim; x++)
+    {   
+        for (int y = int(moves->getKingCoord().y) - 1, lim = y + 3; y < lim; y++)
+        {
+            if (x > 0 && x < Traits::boardSize && y > 0 && y < Traits::boardSize)
+            {
+                currentSquare.x = Traits::Horizontal{x};
+                currentSquare.y = Traits::Vertical{y};
+                pieceOnSquare = board->getPiece(currentSquare);
+
+                if (pieceOnSquare && pieceOnSquare->getColor() != moves->getColor() &&
+                    moves->getKingCoord() != currentSquare &&
+                    !notMoves->isAccessedSquare(currentSquare))
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;   
+}
+
 int Gameplay::start()
 {
     Traits::Coordinates from, to;
-
-    auto moves = &white;
-    auto notMoves = &black;
-
     while (showGoesOn())
     {
         try 
@@ -120,23 +148,62 @@ int Gameplay::start()
             {
                 continue;
             }
-            if (!possibleMove(moves, notMoves, from, to))
+            movingProccess(from, to);
+            if (stalemate)
             {
-                throw std::runtime_error{ "Impossible move" };
+                break;
             }
-            moves->move(from, to);
-            if (isCheckmate(moves, notMoves))
-            {
-                notMoves->setCheckmate(true);
-            }
-            std::swap(moves, notMoves);
         }
         catch (const std::runtime_error& err)
         {
             std::cout << err.what() << std::endl;
             continue;
         }
+        catch (...)
+        {
+            std::cerr << "Something went wrong.";
+            return EXIT_FAILURE;
+        }
+    }
+    endgame();
+    return EXIT_SUCCESS;
+}
 
-        whiteMove = !whiteMove;
+void Gameplay::movingProccess(Traits::Coordinates from, Traits::Coordinates to)
+{
+    auto moves = whiteMove ? &white : &black;
+    auto notMoves = whiteMove ? &black : &white;
+
+    Piece* defeated{ nullptr };
+
+    if (!possibleMove(moves, notMoves, from, to))
+    {
+        throw std::runtime_error{ "Impossible move" };
+    }
+    defeated = moves->move(from, to);
+    if (!defeated)
+    {
+        defeated->setCoordinates(Traits::NULLPOS);
+    }
+    if (isCheckmate(moves, notMoves))
+    {
+        notMoves->setCheckmate(true);
+        return;
+    }
+    stalemate = !kingCanMove(moves, notMoves) && !moves->isAbleToMove();
+
+    std::swap(moves, notMoves);
+    whiteMove = !whiteMove;
+}
+
+void Gameplay::endgame() const 
+{
+    if (!showGoesOn())
+    {
+        std::cout << "Congrats, " << (white.isCheckmate() ? " black " : " white ") << "player wins.";
+    }
+    else
+    {
+        std::cout << "Oops, it seems to be draw.";
     }
 }
--- Gameplay.h
+++ Gameplay.h
@@ -13,13 +13,20 @@ class Gameplay
     Player black{ board, Traits::Color::BLACK };
 
     bool whiteMove{ true };
+    bool stalemate{ false };
 
-    bool showGoesOn() { return !white.isCheckmate() && !black.isCheckmate(); }
+    bool showGoesOn() const { return !white.isCheckmate() && !black.isCheckmate(); }
     void inputToMove(Traits::Coordinates& from, Traits::Coordinates& to) const;
 
     bool possibleMove(Player* moves, Player* checks, Traits::Coordinates from, Traits::Coordinates to) const;
-    bool isCheckmate(Player* checks, Player* inCheck);
-    
+    bool isCheckmate(Player* checks, Player* inCheck) const;
+
+    void endgame() const;
+    void movingProccess(Traits::Coordinates from, Traits::Coordinates to);
+
+    std::vector<Traits::Coordinates> allSquaresForKing(const Player* moves, const Player* notMoves) const;
+    bool kingCanMove(const Player* moves, const Player* notMoves) const;
+
     static Traits::Coordinates convertCoordinates(int x, int y) { return { Traits::Horizontal{ x }, Traits::Vertical{ y } }; }
 
 public:
