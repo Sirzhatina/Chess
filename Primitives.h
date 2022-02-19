28a1ec1ce7602253375b5b228a255b93ee42d4e3
Sirzhatina <mazurkevichsergey@gmail.com>
2022-02-20 00:34:25 +0300

    deleted NPOS

--- Primitives.h
+++ Primitives.h
@@ -1,30 +1,22 @@
 //
 // Created by Sirzh on 09.08.2021.
 //
-
 #pragma once
 
 namespace Chess
 {
-
 constexpr auto boardSize = 8;
 
-enum class Horizontal {
-    A, B, C, D, E, F, G, H, NPOS
-};
-enum class Vertical {
-    one, two, three, four, five, six, seven, eight, NPOS
-};
+enum class Horizontal { A,   B,   C,     D,    E,    F,   G,     H     };
+enum class Vertical   { one, two, three, four, five, six, seven, eight };
 
-struct Coordinates {
+struct Coordinates
+{
     Horizontal x;
     Vertical y;
 };
-constexpr Coordinates NULLPOS{ Horizontal::NPOS, Vertical::NPOS };
 inline bool operator==(const Coordinates& c1, const Coordinates& c2) { return c1.x == c2.x && c1.y == c2.y; }
 inline bool operator!=(const Coordinates& c1, const Coordinates& c2) { return !(c1 == c2); }
 
-enum class Color {
-    BLACK, WHITE
-};
+enum class Color { BLACK, WHITE };
 }
