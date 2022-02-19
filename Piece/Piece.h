0b659c93a00d215bae20a2c12ec607158af01889
Sirzhatina <mazurkevichsergey@gmail.com>
2022-02-20 00:32:36 +0300

    totally refactored Piece

--- Piece/Piece.h
+++ Piece/Piece.h
@@ -19,89 +19,21 @@ class Piece
     Coordinates   _currentCoord;
     bool          _firstMove{ true };
 
+protected:
+    bool isFriendlySquare(Coordinates square) const;
+    bool isSameSquare(Coordinates square)     const { return coord() == square; }
+
 public:
     Piece(const Player* p, Coordinates coord);
     virtual ~Piece() = default;
 
-    virtual bool                     possibleMove(Coordinates to)  const = 0;
-    virtual std::vector<Coordinates> squaresBefore(Coordinates to) const = 0;
+    virtual bool                     isPossibleMove(Coordinates to) const = 0;
+    virtual std::vector<Coordinates> squaresBefore(Coordinates to)  const = 0;
     
     void setCoordinates(Coordinates to);
 
     const Player* player()      const { return _player; }
     Coordinates   coord()       const { return _currentCoord; }
     bool          isFirstMove() const { return _firstMove; }
-    bool          isOnBoard()   const { _currentCoord != NULLPOS; }
 };
-
-
-class Pawn: public Piece
-{
-    bool possibleAttack(Coordinates to) const;
-
-public:
-    Pawn(const Player* p, Coordinates coord): Piece(p, coord) { }
-
-    bool possibleMove(Coordinates to) const override;
-    static bool correctRoute(const Pawn& p, Coordinates to);
-    std::vector<Coordinates> squaresBefore(Coordinates to) const override;
-};
-
-
-class Knight: public Piece
-{
-public:
-    Knight(const Player* p, Coordinates coord): Piece(p, coord) { }
-
-    bool possibleMove(Coordinates to) const override;
-    static bool correctRoute(Coordinates from, Coordinates to, const Board* b = nullptr);
-    std::vector<Coordinates> squaresBefore(Coordinates to) const override;
-};
-
-
-class Bishop: public Piece
-{
-public:
-    Bishop(const Player* p, Coordinates coord): Piece(p, coord) { }
-
-    bool possibleMove(Coordinates to) const override;
-    static bool correctRoute(Coordinates from, Coordinates to, const Board* b);
-
-    std::vector<Coordinates> squaresBefore(Coordinates to) const override;
-};
-
-
-class Rook: public Piece
-{
-public:
-    Rook(const Player* p, Coordinates coord): Piece(p, coord) { }
-
-    bool possibleMove(Coordinates to) const override;
-    static bool correctRoute(Coordinates from, Coordinates to, const Board* b);
-
-    std::vector<Coordinates> squaresBefore(Coordinates to) const override;
-};
-
-
-class Queen: public Piece
-{
-public:
-    Queen(const Player* p, Coordinates coord): Piece(p, coord) { }
-
-    bool possibleMove(Coordinates to) const override;
-    static bool correctRoute(Coordinates from, Coordinates to, const Board* b);
-
-    std::vector<Coordinates> squaresBefore(Coordinates to) const override;
-};
-
-
-class King: public Piece
-{
-public:
-    King(const Player* p, Coordinates coord): Piece(p, coord) { }
-
-    bool possibleMove(Coordinates to) const override;
-    static bool correctRoute(const King& k, Coordinates from, Coordinates to);
-    std::vector<Coordinates> squaresBefore(Coordinates to) const override;
-};
-} // ends namespace Chess
+}
