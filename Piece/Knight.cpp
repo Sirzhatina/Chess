75ebbe2e8a8fe8e9b4cea65adb43055d64d0a1b1
Sirzhatina <mazurkevichsergey@gmail.com>
2022-02-21 16:34:17 +0300

    dummy changes

--- Piece/Knight.cpp
+++ Piece/Knight.cpp
@@ -10,12 +10,12 @@ bool Knight::isPossibleMove(Coordinates to) const
         if ((std::abs(int(to.y) - int(coord().y)) == 2 && std::abs(int(to.x) - int(coord().x)) == 1) ||
             (std::abs(int(to.x) - int(coord().x)) == 2 && std::abs(int(to.y) - int(coord().y)) == 1))
         {
-            return true;
+            return !isFriendlySquare(to) && !isSameSquare(to);
         }
         return false;
     };
 
-    return !isFriendlySquare(to) && isValidRoute(to);
+    return isValidRoute(to);
 }
 
 std::vector<Coordinates> Knight::squaresBefore(Coordinates to) const { return Piece::squaresBefore(to); }
