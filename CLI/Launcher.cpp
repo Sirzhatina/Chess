1057037301e70c949880bd206bf6dd87d2f313fa
Sirzhatina <mazurkevichsergey@gmail.com>
2022-10-19 23:41:40 +0300

    Gameplay is initialized and released in Launcher::play() now

--- CLI/Launcher.cpp
+++ CLI/Launcher.cpp
@@ -21,19 +21,18 @@ void Launcher::startMenu()
     while (true)
     {
         _dr->drawMainMenu();
-        (std::cin >> choice).get();
+        std::cin >> choice;
+        while (std::cin.get() != '\n');
 
         switch (choice)
         {
         case '1':
-            _dr->drawPlay();
             play();
             break;
         case '2':
             _dr->drawSettingsMenu();
             break;
         case '3':
-            system("pause");
             return;
         default:
             continue;
@@ -43,13 +42,15 @@ void Launcher::startMenu()
 
 void Launcher::play()
 {
+    _gp = std::make_unique<Chess::Gameplay>(_dr, std::make_shared<InputHandler>());   
     try
     {
         _gp->start();
-    }    
+    }
     catch(const IInputHandler::ExitCase& e)
     {
         std::cerr << e.byeMsg() << '\n';
     }
+    delete _gp.release();
 }
     
