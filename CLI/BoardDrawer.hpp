e96623affd158efc080a86fb91363a7bbf681da3
Sirzhatina <mazurkevichsergey@gmail.com>
2023-02-06 19:33:46 +0300

    BoardDrawer is irrelevant now, Drawer takes its place

--- CLI/BoardDrawer.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <IBoardDrawer.hpp>
-
-class BoardDrawer : public IBoardDrawer
-{
-public:
-    void drawBoard(std::shared_ptr<const Chess::Board> b) const override;
-};
--- CLI/Drawer.hpp
+++ CLI/Drawer.hpp
@@ -1,12 +1,9 @@
 #pragma once
 
 #include <IDrawer.hpp>
-#include "BoardDrawer.hpp"
 
 class Drawer : public IDrawer
 {
-    std::shared_ptr<const BoardDrawer> boardDrawer_{new BoardDrawer{}};
-
     void drawBoard(const Chess::Board& board) const override;
 
     void showRemainingTime(const Chess::Match& m) const;
