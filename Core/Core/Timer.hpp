870bf56d178295c389dcaf20611b6156eed24e08
Sirzhatina <mazurkevichsergey@gmail.com>
2023-02-08 14:22:47 +0300

    Timer got expanded with a few methods and implemented back inside the class

--- Core/Core/Timer.hpp
+++ Core/Core/Timer.hpp
@@ -7,26 +7,88 @@ template <class DurationUnits>
 class Timer
 {
 public:
-    
-    Timer() = default;
 
     template <class Duration = std::chrono::seconds>
-    Timer(Duration&& d = Duration{0});
+    Timer(Duration&& d = Duration{0})
+    : m_expired{true}
+    , m_suspended{false}
+    , m_start{std::chrono::system_clock::time_point{}}
+    , m_duration{std::chrono::duration_cast<DurationUnits>(std::forward<Duration&&>(d))}
+    {
 
-    auto& operator=(Timer&& t);
+    }
 
-    // It's not allowed to launch timer twice until it's finished - in that case, the function returns false; otherwise true
-    bool start();
+    auto& operator=(Timer&& t)
+    {
+        m_expired  = t.m_expired;
+        m_start    = t.m_start;
+        m_duration = t.m_duration;
 
-    void stop();
+        return *this;
+    }
 
-    void wait();
+    void start() 
+    {
+        std::lock_guard lock{m_mtx};
+        if (m_expired)
+        {
+            m_start = std::chrono::system_clock::now(); 
+            m_expired = false;
+        }
+    }
 
-    DurationUnits remainingTime();
+    void stop()
+    {
+        m_start = {};
+        m_expired = true;
+    }
+
+    bool isExpired()   const { return m_expired;   }
+    bool isSuspended() const { return m_suspended; }
+
+    void suspend() { m_suspended = true;  }
+    void resume()  
+    {
+        if (!m_expired && m_suspended)
+        {
+            m_start = std::chrono::system_clock::now(); 
+            m_suspended = false; 
+        }
+    }
+
+    void wait() 
+    {
+        // waiting for suspended timer lasts forever and essentially doesn't make sense
+        if (m_suspended) return;
+        if (!m_expired)
+        {
+            m_duration =- remainingImpl();
+            std::this_thread::sleep_for(m_duration <= 0 ? DurationUnits{0} : m_duration);
+            m_expired = true;
+        }
+    }
+
+    DurationUnits remainingTime()
+    {
+        if (m_suspended)
+        {
+            return m_duration;
+        }
+        if (!m_expired)
+        {
+            m_duration =- remainingImpl();
+            
+            m_expired = m_duration <= DurationUnits{0};
+
+            return m_expired ? DurationUnits{0} : m_duration;
+        }
+        return DurationUnits{0};
+    }
 
 private:
 
     bool m_expired;
+    bool m_suspended;
     std::chrono::system_clock::time_point m_start;
     DurationUnits                         m_duration;
 
@@ -37,74 +99,3 @@ private:
         return std::chrono::duration_cast<DurationUnits>(std::chrono::system_clock::now() - m_start);
     }
 };
-
-/*
- * ***********************************************************************************************
- * ********                             I M P L E M E N T A T I O N                         ******
- * ***********************************************************************************************
-*/
-
-template <class DurationUnits>
-template <class Duration>
-Timer<DurationUnits>::Timer(Duration&& d)
-: m_expired{true}
-, m_start{std::chrono::system_clock::time_point{}}
-, m_duration{std::chrono::duration_cast<DurationUnits>(std::forward<Duration&&>(d))}
-{
-
-}
-template <class DurationUnits>
-auto& Timer<DurationUnits>::operator=(Timer&& t)
-{
-    m_expired  = t.m_expired;
-    m_start    = t.m_start;
-    m_duration = t.m_duration;
-
-    return *this;
-}
-
-template <class DurationUnits>
-bool Timer<DurationUnits>::start() 
-{
-    std::lock_guard lock{m_mtx};
-    if (m_expired)
-    {
-        m_start = std::chrono::system_clock::now(); 
-        m_expired = false;
-        
-        return true;
-    }
-    return false;
-}
-
-template <class DurationUnits>
-void Timer<DurationUnits>::stop()
-{
-    m_start = {};
-    m_expired = true;
-}
-
-template <class DurationUnits>
-void Timer<DurationUnits>::wait() 
-{ 
-    if (!m_expired)
-    {
-        m_duration =- remainingImpl();
-        std::this_thread::sleep_for(m_duration <= 0 ? DurationUnits{0} : m_duration);
-        m_expired = true;
-    }
-}
-
-template <class DurationUnits>
-DurationUnits Timer<DurationUnits>::remainingTime()
-{
-    if (!m_expired)
-    {
-        m_duration =- remainingImpl();
-        
-        m_expired = m_duration <= 0;
-
-        return m_expired ? DurationUnits{0} : m_duration;
-    }
-    return DurationUnits{0};
-}
