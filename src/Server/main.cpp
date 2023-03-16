168469ee5bb07946a3d1c18f504b185e8cc99a9b
Sirzhatina <mazurkevichsergey@gmail.com>
2023-03-16 21:59:25 +0300

    command line args handling

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -1,10 +1,14 @@
-#include <future>
+#include <iostream>
 #include <string>
 #include "Server.hpp"
 
 int main(int argc, char* argv[])
 {
-
+    if (argc < 2)
+    {
+        std::cerr << "Fatal error: port value is not provided. Program usage: <program> <port>.";
+        return EXIT_FAILURE;
+    }
     Server server{std::stoi(argv[1])};
 
     server.run();
